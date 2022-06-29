66c1ac0fafafbddba99bdd6256437a3c353b9218
Shubham Jain <ershubham73325@gmail.com>
2022-06-30 02:17:55 +0530

    Update the base Urls

--- DRF/urls.py
+++ DRF/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns=[
-  # path('user_details/', views.userdetails),
+  path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+  path('user_details/', views.userdetails),
+  path('userid_details/<int:pk>/', views.userid_details),
+  path('profession/', views.professiondetails),
+  path('professionid_details/<int:pk>/', views.professionid_details),
+  path('add_user/', views.add_user),
+  path('add_profession/', views.add_profession),
+  path('edit_user/<int:pk>/', views.edit_user),
+  path('edit_profession/<int:pk>/', views.edit_profession),
+  path('delete_user/<int:pk>/', views.delete_user),
+  path('delete_profession/<int:pk/', views.delete_profession),
 ]
--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -14,24 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from DRF import views
-from rest_framework_simplejwt import views as jwt_views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('user_details/', views.userdetails),
-    path('userid_details/<int:pk>/', views.userid_details),
-    path('profession/', views.professiondetails),
-    path('professionid_details/<int:pk>/', views.professionid_details),
-    path('add_user/', views.add_user),
-    path('add_profession/', views.add_profession),
-    path('edit_user/<int:pk>/', views.edit_user),
-    path('edit_profession/<int:pk>/', views.edit_profession),
-    path('delete_user/<int:pk>/', views.delete_user),
-    path('delete_profession/<int:pk/', views.delete_profession),
+    path('', include('DRF.urls')),
 ]
